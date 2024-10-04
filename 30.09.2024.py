e02183e47fab8df20402476784cda9d42edf62f0
kristiine302 <kpujate7@gmail.com>
2024-10-04 11:39:05 +0300

    04,10,2024 darbs 11a

---
+++ 30.09.2024.py
@@ -0,0 +1,21 @@
+a = 5
+b = 10
+
+print(a + b)
+print(a - b)
+print(a * b)  
+print(a / b)
+print(a ** b)
+print(a // b)
+print(a % b)
+
+a = int(input('Enter number a: ')
+
+print(a + b)
+print(a - b)
+print(a * b)  
+print(a / b)
+print(a ** b)
+print(a // b)
+print(a % b)
+    
